c6c97551b2deb9b73d3a4fe681ab4431bf8f1ee9
Grek Maria <grekmariaa@gmail.com>
2022-11-04 20:33:35 +0200

    Create Reviews.jsx

---
+++ src/components/Reviews.jsx
@@ -0,0 +1,50 @@
+import { useParams, useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+import { reviewsRequest } from '../components/api';
+import { ReviewsInfoList } from './Reviews.styles';
+//компонент Reviews, інформація про огляди. Рендериться на сторінці MovieDetails.
+
+const Reviews = () => {
+  const { movieId } = useParams();
+  const [reviewsInfo, setReviewsInfo] = useState([]);
+  const location = useLocation();
+  console.log('location in Reviews', location.state.from);
+
+  useEffect(() => {
+    if (!movieId) {
+      return;
+    }
+
+    (async function reviewsInfo() {
+      const res = await reviewsRequest(movieId);
+      setReviewsInfo(res.data.results);
+    })();
+
+    return () => {
+      // second
+    };
+  }, [movieId]);
+
+  return (
+    <>
+      {reviewsInfo.length === 0 ? (
+        <div>We don`t have information for this movie</div>
+      ) : (
+        <ReviewsInfoList>
+          {reviewsInfo.map(({ id, author, content }) => (
+            <li key={id}>
+              <p>
+                <b>Author: </b>
+                {author}
+              </p>
+              <p>{content}</p>
+            </li>
+          ))}
+        </ReviewsInfoList>
+      )}
+    </>
+  );
+};
+
+export default Reviews;
