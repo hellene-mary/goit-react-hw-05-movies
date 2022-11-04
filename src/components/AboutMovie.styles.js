2a2a92ffabca1baed26c78f862aade74cc7859fa
Grek Maria <grekmariaa@gmail.com>
2022-11-04 20:33:18 +0200

    Create AboutMovie.styles.js

---
+++ src/components/AboutMovie.styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  display: flex;
+  gap: 30px;
+  border-bottom: 1px solid #727272;
+  padding: 15px 0;
+
+  img {
+    width: 300px;
+    height: auto;
+  }
+
+  p {
+    padding-top: 8px;
+  }
+
+  h2 {
+    margin-top: 10px;
+  }
+
+  ul {
+    padding-left: 30px;
+    padding-top: 10px;
+  }
+`;
