34201699c737909a17f11e2be972013494fc0727
Grek Maria <grekmariaa@gmail.com>
2022-11-04 20:33:45 +0200

    Create MovieDetails.jsx

---
+++ src/pages/MovieDetails.jsx
@@ -0,0 +1,72 @@
+import { Suspense, useState, useEffect } from 'react';
+import { Outlet, useParams, useLocation } from 'react-router-dom';
+import { HiChevronLeft } from 'react-icons/hi';
+
+import {
+  Container,
+  MoreInfoLink,
+  MoreInfoList,
+  Box,
+  GoBack,
+} from './../components/Layout.styles';
+import { movieInfoRequest } from '../components/api';
+import { AboutMovie } from '../components/AboutMovie';
+
+// /сторінка з детальною інформацією про кінофільм
+// !add spiner
+const MovieDetails = () => {
+  const [movieInfo, setMovieInfo] = useState([]);
+  const { movieId } = useParams();
+  const location = useLocation();
+  const backLinkHref = location.state?.from ?? '/';
+  console.log('backLinkHref', backLinkHref);
+
+  useEffect(() => {
+    if (!movieId) {
+      return;
+    }
+
+    (async function aboutMovie() {
+      const res = await movieInfoRequest(movieId);
+      if (res.data.length === 0) {
+        return;
+      }
+      setMovieInfo(res.data);
+    })();
+
+    return () => {
+      // second
+    };
+  }, [movieId]);
+
+  return (
+    <Container>
+      <GoBack to={backLinkHref}>
+        <HiChevronLeft />
+        Go back
+      </GoBack>
+
+      <AboutMovie movieInfo={movieInfo} />
+
+      <Box>
+        <p>Additional information</p>
+        <MoreInfoList>
+          <li>
+            <MoreInfoLink to={'cast'}>Cast</MoreInfoLink>
+          </li>
+          <li>
+            <MoreInfoLink to={'reviews'} state={{ from: location }}>
+              Reviews
+            </MoreInfoLink>
+          </li>
+        </MoreInfoList>
+      </Box>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Outlet />
+      </Suspense>
+    </Container>
+  );
+};
+
+export default MovieDetails;
