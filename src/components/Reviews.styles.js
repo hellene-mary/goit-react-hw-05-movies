8d2d873816edf883ec4e1fd6e2c7e243f5f840cd
Grek Maria <grekmariaa@gmail.com>
2022-11-04 20:33:37 +0200

    Create Reviews.styles.js

---
+++ src/components/Reviews.styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const ReviewsInfoList = styled.ul`
+  padding: 20px 45px;
+  margin: 0;
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+
+  p {
+    margin-bottom: 10px;
+  }
+`;
